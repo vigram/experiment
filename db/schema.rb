df4a729acefcaf18e0c6a9b68dbd7c88c2492903
vigram <vigram.k@gmail.com>
2012-12-18 16:21:16 +0530

    Experiment for deploy script

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120815165558) do
+ActiveRecord::Schema.define(:version => 20121218093831) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -27,6 +26,13 @@ ActiveRecord::Schema.define(:version => 20120815165558) do
     t.datetime "updated_at",  :null => false
     t.integer  "category_id"
     t.string   "type"
+  end
+
+  create_table "versions", :force => true do |t|
+    t.string   "version"
+    t.datetime "installed_dt"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
 end
